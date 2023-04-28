2f3e0e26145f5b669c920fb8b5565188f7e06b2b
Morris <tmorris@walkerinfo.com>
2023-04-28 07:31:15 -0700

    fine tuning script added. Conversation history added.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import slack
 import openai
 import os
+import json
 from pathlib import Path
 from dotenv import load_dotenv
 from flask import Flask
@@ -28,8 +29,11 @@ last_msg = ''
 @slack_event_adapter.on('message')
 def message(payload):
     event = payload.get('event', {})
+    # print("event:", event)
     channel_id = event.get('channel')
     print("channel:", channel_id)
+    channel_type = event.get('channel_type')
+    print("channel type:", channel_type)
     user_id = event.get('user')
     text = event.get('text')
     print("user msg:", text)
@@ -39,18 +43,69 @@ def message(payload):
     else:
         last_msg = text
     if user_id != BOT_ID:
+        # SEEDED CHAT OPTION
+        full_msgs = construct_chat_history(user_id, text)
+        print("full message with history:", full_msgs)
         completion = openai.ChatCompletion.create(
             model='gpt-3.5-turbo',
-            # model='gpt-4-0314',
-            messages=[{"role": "system", "content": "You are a Qualtrics assistant. You will answer questions about the setup and functionality of the survey platform Qualtrics, and you will do so as accurately and concisely as you can. Reply with OK if you understand."},
+            # model='curie:ft-personal-2023-04-28-05-11-33',
+            messages=[{"role": "system", "content": "You are a Qualtrics assistant. You will ONLY answer questions about the setup and functionality of the survey platform Qualtrics, and you will do so as accurately and concisely as you can. You will refuse to answer any questions unrelated to Qualtrics. Reply with OK if you understand."},
                       {"role": "assistant", "content": "OK"},
                       {"role": "user", "content": text}]
         )
-
         resp = completion.choices[0].message.content
+
+        # FINE-TUNED SINGLE COMPLETION OPTION
+        # completion = openai.Completion.create(
+        #     model='curie:ft-personal-2023-04-28-05-11-33',
+        #     prompt=text,
+        #     max_tokens=1000
+        # )
+        # resp = completion.choices[0].text
+
         print(resp)
         client.chat_postMessage(channel=channel_id,
                                 text=resp)
+        append_and_save_conversation(user_id, text, resp)
+
+
+def construct_chat_history(uuid, chat):
+    # must return an array of the chat history
+    data = load_or_create_json_file(uuid)
+    base = [{"role": "system", "content": "You are a Qualtrics assistant. You will ONLY answer questions about the setup and functionality of the survey platform Qualtrics, and you will do so as accurately and concisely as you can. You will refuse to answer any questions unrelated to Qualtrics. Reply with OK if you understand."},
+            {"role": "assistant", "content": "OK"}]
+    if len(data) > 0:
+        base += data
+    new = {'role': 'user', 'content': chat}
+    base.append(new)
+    return base
+
+
+def load_or_create_json_file(user_id):
+    file_name = f"conversations/{user_id}.json"
+    # Write the empty file if it doesn't exist
+    if not os.path.exists(file_name):
+        with open(file_name, "w") as json_file:
+            json.dump([], json_file)
+    # Read the file data
+    with open(file_name, "r") as json_file:
+        data = json.load(json_file)
+
+    return data
+
+
+def append_and_save_conversation(user_id, user_string, bot_string):
+    data = load_or_create_json_file(user_id)
+    file_name = f"conversations/{user_id}.json"
+
+    user_message = {"role": "user", "content": user_string}
+    bot_message = {"role": "assistant", "content": bot_string}
+
+    data.append(user_message)
+    data.append(bot_message)
+
+    with open(file_name, "w") as json_file:
+        json.dump(data, json_file)
 
 
 if __name__ == "__main__":
--- training_upload.py
+++ training_upload.py
@@ -2,6 +2,7 @@ import openai
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from time import sleep
 
 envpath = Path('.') / '.env'
 load_dotenv(dotenv_path=envpath)
@@ -10,11 +11,28 @@ load_dotenv(dotenv_path=envpath)
 openai.api_key = os.environ['OPENAI_KEY']
 abpath = Path('training_data.jsonl').absolute()
 
-resp = openai.FineTune.create(
-    n_epochs=20,
-    batch_size=100,
-    training_file=os.path.abspath(abpath)
-)
+# upld = openai.File.create(
+#     file=open("training_data.jsonl", 'rb'),
+#     purpose="fine-tune"
+# )
 
-job_id = resp['id']
-print(f"fine tuning job created with ID: {job_id}")
+# print(upld)
+# myid = upld['id']
+# print("myid:", myid)
+
+# resp = openai.FineTune.create(training_file=myid)
+
+# print(resp)
+# job_id = resp['id']
+# print(f"fine tuning job created with ID: {job_id}")
+
+job_id = "ft-vxLGQcDdSQBEAUC0kw0Gjhuf"
+finished = False
+while not finished:
+    mdl = openai.FineTune.retrieve(id=job_id)
+
+    print(mdl)
+    print("model:", mdl['fine_tuned_model'])
+    if not mdl['fine_tuned_model'] == None:
+        finished = True
+    sleep(5)
